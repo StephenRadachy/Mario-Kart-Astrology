7e3644ee73780d51626bf456dbc7a38122f7ed1d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-01 19:23:37 +0000

    Deploy StephenRadachy/Mario-Kart-Astrology to github.com/StephenRadachy/Mario-Kart-Astrology.git:gh-pages

---
+++ precache-manifest.467133d4e146e942c5dcf0f0c780ca48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ca93238ab908b18fb54",
+    "url": "/Mario-Kart-Astrology/static/css/main.8e312d98.chunk.css"
+  },
+  {
+    "revision": "2ca93238ab908b18fb54",
+    "url": "/Mario-Kart-Astrology/static/js/main.7da636a7.chunk.js"
+  },
+  {
+    "revision": "be1c7bc1a472b1f676fa",
+    "url": "/Mario-Kart-Astrology/static/js/runtime~main.3434a78e.js"
+  },
+  {
+    "revision": "1cf3289d1fb2f3af18ed",
+    "url": "/Mario-Kart-Astrology/static/js/2.ac5716ec.chunk.js"
+  },
+  {
+    "revision": "918d0da246abf330237dba78bd09ffb3",
+    "url": "/Mario-Kart-Astrology/index.html"
+  }
+];
